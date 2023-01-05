270692e3ae26146674c1cee95c8034a8f1be2f7b
Alfian Rudi Yogatama <alfianrudiyo@gmail.com>
2023-01-05 13:41:48 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const timeout = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+/**
+ * ? Solve this problem :
+ * * we need to do some very basic loop
+ * * what make it`s more interesting is we need to run some 2 seconds "delayed" if the number is odd number
+ * * you must use "timeout" function above
+ */
+
+const start = (array) => {
+  return 'ok'
+}
+
+const arr = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30
+]
+
+start(arr)
+
